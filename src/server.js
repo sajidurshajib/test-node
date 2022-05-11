6bb22f7ebf4d2686c3e6d9a3c96e8b8e2b7d65d3
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-11 23:59:34 +0600

    connect: mongo

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
-
+const mongoose = require('mongoose')
 const app = require('./app')
+
+// mongodb connection
+mongoose
+    .connect(process.env.DATABASE_LOCAL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        console.log('DB connection successfull!')
+    )
 
 // port for server
 const port = process.env.PORT || 8000
